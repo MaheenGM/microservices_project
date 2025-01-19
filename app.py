053379dfd7b09a22ed86a818502ffdf6174b8e41
MaheenGM <maheenghulammuhammad@gmail.com>
2025-01-19 15:19:14 +0500

    Add files via upload
    files to be kept in "user-folder"

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-# In-memory book storage
-books = []
+# In-memory user storage (this can be a database in a real application)
+users = []
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Welcome to the Book Service!"
+    return "Welcome to the User Service!"
 
-@app.route('/books', methods=['GET'])
-def get_books():
-    return jsonify(books)
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
 
-@app.route('/books', methods=['POST'])
-def create_book():
-    new_book = request.get_json()  # Expecting JSON data in request body
-    books.append(new_book)  # Add the new book to the list
-    return jsonify(new_book), 201  # Return created book with a 201 status
+@app.route('/users', methods=['POST'])
+def create_user():
+    new_user = request.get_json()  # Expecting JSON data in request body
+    users.append(new_user)  # Add the new user to the list
+    return jsonify(new_user), 201  # Return created user with a 201 status
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5003)
